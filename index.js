8f52fbf220313c3e6096107f04d86069230c86ca
Dario Vladovic <d.vladimyr@gmail.com>
2017-03-15 13:53:37 +0100

    :goat: I can haz animations

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 const plyr = require('plyr');
 const Choices = require('choices.js');
 const urlJoin = require('url-join');
+const fade = require('fade');
 const stations = require('./stations.json');
+
+let $app = document.body;
 
 let $player = plyr.setup('.radio-player', {
   controls: [ 'play', 'mute', 'volume' ],
@@ -13,12 +16,14 @@ let $player = plyr.setup('.radio-player', {
 let $picker = new Choices('.station-selector', { choices: stations });
 setStation($player, $picker.passedElement.value);
 
+fade.in($app);
+
 $picker.passedElement.addEventListener('change', e => {
   setStation($player, e.target.value);
   $player.stop();
 });
 
 function setStation(player, streamUrl) {
-  let src = urlJoin(streamUrl, '/;')
+  let src = urlJoin(streamUrl, '/;');
   player.source({ type: 'audio', sources: [{ src }] });
 }
