bed5c379c2ab997a9711f532664b0a55ade67c17
Dario Vladovic <d.vladimyr@gmail.com>
2017-03-20 22:42:04 +0100

    :tada: Enable direct linking to station
    - implementing client side routing using 200.html (surge)
    - adding station paths

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let $player = plyr.setup('.radio-player', {
   keyboardShortcuts: { focused: true, global: true }
 })[0];
 
+let path = getPath(window.location);
+let selectedStation = findStation(stations, path);
+selectedStation.selected = true;
+
 let $picker = new Choices('.station-selector', { choices: stations });
 setStation($player, $picker.passedElement.value);
 
@@ -26,4 +30,19 @@ $picker.passedElement.addEventListener('change', e => {
 function setStation(player, streamUrl) {
   let src = urlJoin(streamUrl, '/;');
   player.source({ type: 'audio', sources: [{ src }] });
+}
+
+function getPath(location=window.location) {
+  return location.pathname.replace(/^\//, '');
+}
+
+function findStation(stations, path='') {
+  return stations.find(it => {
+    if (!it.path) return false;
+    return equals(it.path, path);
+  }) || stations[0];
+}
+
+function equals(str1, str2) {
+  return str1.trim().toLowerCase() === str2.trim().toLowerCase();
 }
