c2d64e16eebe55c6c1a7df6b65095fa43ec32cbe
Dario Vladovic <d.vladimyr@gmail.com>
2018-01-05 17:45:22 +0100

    Decouple state from data :wrench:

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const App = (new class {
     const stationUrl = window.localStorage.getItem('stationUrl');
     const byStationUrl = it => it.url === stationUrl;
     const selectedStation = stations.filter(byStationUrl)[0] || stations[0];
-    this.station = this.setStation(selectedStation);
+    this.setStation(selectedStation);
     this.stationPicker = this.setupStationPicker('.station-picker', stations);
 
     fade.in(this.el);
@@ -40,7 +40,7 @@ const App = (new class {
     const choices = stations.map(it => ({
       label: it.name,
       value: it.url,
-      selected: it.selected,
+      selected: it === this.station,
       customProperties: { station: it }
     }));
     const $picker = new Choices(selector, { choices });
@@ -59,10 +59,8 @@ const App = (new class {
   setStation(station) {
     this.player.source({ type: 'audio', sources: [{ src: station.url }]});
     document.title = `${station.name} - Radiocloud`;
-    if (this.station) this.station.selected = false;
+    window.localStorage.setItem('stationUrl', station.url);
     this.station = station;
-    this.station.selected = true;
-    window.localStorage.setItem('stationUrl', this.station.url);
   }
 }(document.body));
 
